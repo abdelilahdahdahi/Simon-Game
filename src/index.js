9c94ddc2b9076a29c415a86cfdb090ec44dec2a7
abdelilah <dahdahiabdelilah2002@gmail.com>
2024-03-30 10:42:35 +0100

    fix sound path

--- src/index.js
+++ src/index.js
@@ -90,7 +90,7 @@ function animatePress(currentColor) {
 
 // sound function
 function playSound(name) {
-  var audio = new Audio("sounds/" + name + ".mp3");
+  var audio = new Audio("../sounds/" + name + ".mp3");
   audio.play();
 }
 
